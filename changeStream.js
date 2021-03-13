7c32583b9c87f6821773d50006104e567d0e525c
Pradyumn Sharma <PradyumnSharma@users.noreply.github.com>
2021-03-13 15:39:02 +0530

    Add files via upload

--- changeStream.js
+++ changeStream.js
@@ -8,8 +8,20 @@ async function execute () {
 
     const database = client.db('training');
     const collection = database.collection('persons');
+
+    const pipeline = [
+        {$match: {operationType: 'delete'} },
+        {$project: {documentKey: true, _id: true}}
+    ];
+
+    const resumeToken = {
+            _data: '82604AEFD00000000F2B022C0100296E5A1004787EF1B3DD424CF9BC78D0FB7041734646645F69640064604AEF890E13F9D4CA138B540004'
+    };
+
     // const changeStream = collection.watch ();
-    const changeStream = collection.watch ({ fullDocument: 'updateLookup' });
+    // const changeStream = collection.watch ({ fullDocument: 'updateLookup' });
+    // const changeStream = collection.watch (pipeline, { fullDocument: 'updateLookup' });
+    const changeStream = collection.watch (pipeline, { fullDocument: 'updateLookup', resumeAfter: resumeToken });
     changeStream.on ('change', next => processDocument(next));
     // const next = await changeStream.next ();
 
